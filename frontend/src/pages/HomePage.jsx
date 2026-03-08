657ea588e51499a0c32d48eddceb74222ae2503e
chien78 <nchien0708@gmail.com>
2026-03-08 22:02:37 +0700

    feat: test frontend connection to backend

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,4 +1,14 @@
+import { useEffect } from "react";
+
 function HomePage() {
+
+  useEffect(() => {
+    fetch("http://localhost:8080")
+      .then(res => res.text())
+      .then(data => console.log("Backend response:", data))
+      .catch(err => console.error("Error connecting to backend:", err));
+  }, []);
+
   return (
     <div>
       <h3>Home Page</h3>
