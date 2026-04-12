5c032abf5846e9fb547cbae068a6d58aa1be3bbb
chien78 <nchien0708@gmail.com>
2026-04-12 21:04:19 +0700

    feat(routes): integrate dashboard layout and protected routes

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,5 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
+import DashboardLayout from "../layouts/DashboardLayout";
+
 import HomePage from "../pages/HomePage";
 import LoginPage from "../pages/LoginPage";
 import RegisterPage from "../pages/RegisterPage";
@@ -9,6 +11,9 @@ import DoctorListPage from "../pages/DoctorListPage";
 import BookingPage from "../pages/BookingPage";
 import MyAppointmentsPage from "../pages/MyAppointmentsPage";
 import MedicalRecordPage from "../pages/MedicalRecordPage";
+
+//12.04
+import CreateMedicalRecordPage from "../pages/CreateMedicalRecordPage";
 
 // dashboard tạm
 function PatientPage() {
@@ -26,9 +31,13 @@ function AppRoutes() {
           <Route path="login" element={<LoginPage />} />
           <Route path="register" element={<RegisterPage />} />
 
+          {/* DASHBOARD */}
+          <Route path="dashboard" element={<DashboardLayout />} />
+
           {/* PATIENT */}
           <Route path="patient" element={<PatientPage />} />
           <Route path="medical-records" element={<MedicalRecordPage />} />
+          <Route path="doctor/create-record" element={<CreateMedicalRecordPage />} />
 
           {/* BOOKING FLOW */}
           <Route path="doctors" element={<DoctorListPage />} />
